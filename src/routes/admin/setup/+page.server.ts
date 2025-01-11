fb7ac09143eee81039e6900072c7549c0209b4ca
Klee Thomas <klee.ut@gmail.com>
2025-01-11 16:48:43 +1100

    :sparkle: introduces basic hand rolled dependency injection

--- src/routes/admin/setup/+page.server.ts
+++ src/routes/admin/setup/+page.server.ts
@@ -1,22 +1,11 @@
 import { fail } from '@sveltejs/kit';
-import { QuestionService } from '$lib/services';
-import type { Actions, PageServerLoad, RequestEvent } from './$types';
+import type { Actions, PageServerLoad } from './$types';
 import { QuestionId } from '$lib/services/question/question-id';
-import { D1QuestionRepository } from '$lib/services/question/d1-question-repository';
-
-function setup(event: RequestEvent): { service: QuestionService } {
-	const env = event.platform?.env;
-	if (!env) {
-		throw new Error('nope');
-	}
-	const questionRepository = new D1QuestionRepository(env.DB);
-	const service = new QuestionService(questionRepository);
-	return { service };
-}
+import { setup } from '../../context';
 
 export const load: PageServerLoad = async (event) => {
-	const { service } = setup(event);
-	const questions = await service.allQuestions();
+	const { questionService } = setup(event.platform);
+	const questions = await questionService.allQuestions();
 	return {
 		questions: questions.map((x) => ({
 			...x,
@@ -28,23 +17,23 @@ export const load: PageServerLoad = async (event) => {
 export const actions = {
 	addQuestion: async (event) => {
 		console.log('Adding question');
-		const { service } = setup(event);
+		const { questionService } = setup(event.platform);
 		const data = await event.request.formData();
 		const questionText = data.get('questionText')?.valueOf() as string | undefined;
 		if (!questionText) {
 			return fail(400, { questionText, missing: true });
 		}
-		await service.addQuestion(questionText);
+		await questionService.addQuestion(questionText);
 	},
 
 	deleteQuestion: async (event) => {
-		const { service } = setup(event);
+		const { questionService } = setup(event.platform);
 
 		const data = await event.request.formData();
 		const questionId = data.get('questionId')?.valueOf() as string | undefined;
 		if (!questionId) {
 			fail(400, { questionId, missing: true });
 		}
-		await service.deleteQuestion(new QuestionId(questionId!));
+		await questionService.deleteQuestion(new QuestionId(questionId!));
 	}
 } satisfies Actions;
