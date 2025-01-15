0f8899bd2d4814a16c87ec40625be36c109dce5d
Klee Thomas <klee.ut@gmail.com>
2025-01-15 19:02:02 +1100

    :lipstick: moving categories to right side

--- src/routes/admin/review/+page.svelte
+++ src/routes/admin/review/+page.svelte
@@ -1,61 +1,67 @@
 <script lang="ts">
 	import Heading from '$lib/components/heading.svelte';
+	import Margin from '$lib/components/margin.svelte';
 	import TextInput from '$lib/components/text-input.svelte';
+	import Category from './category.svelte';
 
 	const { data } = $props();
 </script>
 
 <div class="container">
-	<section>
-		<Heading level="1" size="2">{data.question.questionText}</Heading>
-		<nav class="question_nav">
-			{#if data.previousQuestion}
-				<span>
-					<a href="/admin/review?questionId={data.previousQuestion.questionId}">
-						⬅️ {data.previousQuestion.questionText}
-					</a>
-				</span>
-			{/if}
-			|
-			{#if data.nextQuestion}
-				<span>
-					<a href="/admin/review?questionId={data.nextQuestion.questionId}">
-						{data.nextQuestion.questionText} ➡️
-					</a>
-				</span>
-			{/if}
-		</nav>
-		<section>
-			<Heading level="2" size="4">Top categories</Heading>
-			<div class="categories">
-				<div class="category"><span>Category 1 </span><span> 80</span></div>
-				<div class="category"><span>Category 2 </span><span> 65</span></div>
-				<div class="category"><span>Category 3 </span><span> 53</span></div>
-				<div class="category"><span>Category 4 </span><span> 45</span></div>
-				<div class="category"><span>Category 5 </span><span> 30</span></div>
-				<div class="category"><span>Category 6 </span><span> 20</span></div>
-				<div class="category"><span>Category 7 </span><span> 10</span></div>
-				<div class="category"><span>Category 8 </span><span> 8</span></div>
-			</div>
-		</section>
-		<hr />
-		<table>
-			<thead>
-				<tr>
-					<th>Answer</th><th>Count</th><th>Group</th>
-				</tr>
-			</thead>
-			<tbody>
-				{#each data.question.answers as answer}
+	<Heading level="1" size="2">{data.question.questionText}</Heading>
+	<nav class="question_nav">
+		{#if data.previousQuestion}
+			<span>
+				<a href="/admin/review?questionId={data.previousQuestion.questionId}">
+					⬅️ {data.previousQuestion.questionText}
+				</a>
+			</span>
+		{/if}
+		&nbsp;
+		{#if data.nextQuestion}
+			<span>
+				<a href="/admin/review?questionId={data.nextQuestion.questionId}">
+					{data.nextQuestion.questionText} ➡️
+				</a>
+			</span>
+		{/if}
+	</nav>
+	<Margin marginTop="1">
+		<section class="main">
+			<table>
+				<thead>
 					<tr>
-						<td>{answer.answer}</td><td>{answer.count}</td><td
-							><TextInput value={answer.answer} /></td
-						>
+						<th>Answer</th><th>Count</th><th>Group</th>
 					</tr>
-				{/each}
-			</tbody>
-		</table>
-	</section>
+				</thead>
+				<tbody>
+					{#each data.question.answers as answer}
+						<tr>
+							<td>{answer.answer}</td><td>{answer.count}</td>
+							<td>
+								<TextInput value={answer.answer} />
+							</td>
+						</tr>
+					{/each}
+				</tbody>
+			</table>
+			<section>
+				<Heading level="2" size="4">Top categories</Heading>
+				<Margin marginTop="1">
+					<div class="categories">
+						<Category categoryName="Category 1" score={80} />
+						<Category categoryName="Category 2" score={65} />
+						<Category categoryName="Category 3" score={53} />
+						<Category categoryName="Category 4" score={45} />
+						<Category categoryName="Category 5" score={30} />
+						<Category categoryName="Long category name" score={20} />
+						<Category categoryName="Category 7" score={10} />
+						<Category categoryName="Category 8" score={8} />
+					</div>
+				</Margin>
+			</section>
+		</section>
+	</Margin>
 </div>
 
 <style>
@@ -64,7 +70,7 @@
 		justify-content: space-between;
 	}
 	.container {
-		width: 80vw;
+		width: 90vw;
 		display: flex;
 		flex-direction: column;
 		margin: auto;
@@ -73,17 +79,16 @@
 		padding: 1rem;
 	}
 	.categories {
-		display: grid;
-		grid-template-columns: 1fr 1fr;
+		display: flex;
+		flex-direction: column;
 		width: 80%;
 		margin: auto;
 		gap: 0.5rem;
 	}
-	.category {
-		display: flex;
-		justify-content: space-between;
-		padding: 0.5rem;
-		border: 2px solid yellow;
-		border-radius: 0.2rem;
+
+	.main {
+		gap: 1rem;
+		display: grid;
+		grid-template-columns: 3fr 1fr;
 	}
 </style>
