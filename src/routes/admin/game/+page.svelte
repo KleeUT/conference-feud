a66cfaa61fcb7421ffb2368d8b71f1a0037371cc
Klee Thomas <klee.ut@gmail.com>
2025-02-16 15:38:50 +1100

    :alembic: trying with my own sender

--- src/routes/admin/game/+page.svelte
+++ src/routes/admin/game/+page.svelte
@@ -1,4 +1,4 @@
-<script>
+<!-- <script>
 	import { onMount } from 'svelte';
 	import { source } from 'sveltekit-sse';
 	let value = source('/admin/game/api')
@@ -17,4 +17,26 @@
 <h1>Game Admin</h1>
 
 {new Date(d['data']).toLocaleTimeString()}
-{JSON.stringify(d)}
+{JSON.stringify(d)} -->
+
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let data = 'not yet';
+	onMount(async () => {
+		const res = await fetch('/admin/game/api');
+		if (!res.ok || !res.body) {
+			data = 'http error' + res.body;
+			throw new Error(`HTTP error! status: ${res.status}`);
+		}
+		const reader = res.body.pipeThrough(new TextDecoderStream()).getReader();
+		while (true) {
+			const { value, done } = await reader.read();
+			if (done) break;
+			console.log('received: ' + value);
+			data = value;
+		}
+	});
+</script>
+
+{data}
--- src/routes/admin/game/api/+server.ts
+++ src/routes/admin/game/api/+server.ts
@@ -1,6 +1,6 @@
-// src/routes/custom-event/+server.js
-import { produce } from 'sveltekit-sse';
-
+// // src/routes/custom-event/+server.js
+// import { produce } from 'sveltekit-sse';
+import { newUUID } from '$lib/utils/uuid';
 /**
  * @param {number} milliseconds
  * @returns
@@ -10,20 +10,67 @@ function delay(milliseconds: number) {
 		setTimeout(resolve, milliseconds);
 	});
 }
-// let data =
-export function POST() {
-	console.log('POST');
-	return produce(async function start({ emit }) {
-		while (true) {
-			console.log('Emitting message');
-			const { error } = emit(
-				'message',
-				JSON.stringify({ data: Date.now(), msg: `the time is ${Date.now()}` })
-			);
-			if (error) {
-				return;
-			}
-			await delay(1000);
-		}
-	});
+// // let data =
+// export function POST() {
+// 	console.log('POST');
+// 	return produce(async function start({ emit }) {
+// 		while (true) {
+// 			console.log('Emitting message');
+// 			const { error } = emit(
+// 				'message',
+// 				JSON.stringify({ data: Date.now(), msg: `the time is ${Date.now()}` })
+// 			);
+// 			if (error) {
+// 				return;
+// 			}
+// 			await delay(1000);
+// 		}
+// 	});
+// }
+
+const streams: Map<string, { stream: TransformStream; writable: WritableStreamDefaultWriter }> =
+	new Map();
+async function myBackendCode() {
+	// other backend code here
+	while (true) {
+		console.log('Emitting message to ' + streams.size + ' streams');
+		await Promise.all(
+			streams.values().map(async (stream) => {
+				try {
+					await stream.writable
+						.write(new TextEncoder().encode(`data: ${Date.now()}\n\n`))
+						.catch(console.error);
+				} catch (e) {
+					console.error(e);
+				}
+			})
+		);
+		await delay(1000);
+	}
 }
+myBackendCode();
+export const GET = () => {
+	const stream = new TransformStream();
+	const writable = stream.writable.getWriter();
+	const id = newUUID();
+	streams.set(id, { stream, writable });
+	writable.closed.then(() => {
+		console.log('Stream closed', id);
+		streams.delete(id);
+	});
+	// stream.readable.getReader().closed.then((e) => {
+	// 	console.log('reader closed', e);
+	// });
+	try {
+		const response = new Response(stream.readable, {
+			headers: {
+				'Content-Type': 'text/event-stream',
+				Connection: 'keep-alive',
+				'Cache-Control': 'no-cache'
+			}
+		});
+		return response;
+	} catch (e) {
+		console.error(e);
+	}
+};
