58b35c2244b62051808e93a34358af81516c873c
Klee Thomas <klee.ut@gmail.com>
2025-05-24 11:59:05 +1000

    :alembic: attempting adding retry with jitter to D1 calls

--- src/lib/utils/retry.ts
+++ src/lib/utils/retry.ts
@@ -1,10 +1,12 @@
-export function retry<t>(action: () => t, count: number = 1): t {
+export async function retry<t>(action: () => t, count: number = 1): Promise<t> {
 	try {
 		return action();
 	} catch (e) {
 		console.error({ e, retryCount: count });
 		if (count > 0) {
-			return retry(action, count - 1);
+			return await new Promise((resolve) => {
+				setTimeout(() => resolve(retry(action, count - 1)), Math.floor(Math.random() * 200));
+			});
 		}
 		throw e;
 	}
